d1a057b79cbafec62a91113cf2b0599442a2d7fa
HAL-Kubodera <ths30024@ths.hal.ac.jp>
2026-06-15 11:47:37 +0900

    移動から攻撃にいけるように

--- DECO-Min/Assets/Scripts/GameObject/Character/Player/State/Attack/PlayerAttack.cs
+++ DECO-Min/Assets/Scripts/GameObject/Character/Player/State/Attack/PlayerAttack.cs
@@ -7,11 +7,17 @@ public class PlayerAttack : PlayerState
 
     [SerializeField] private PlayerMove _playerMove;
 
-    public float _frameCounter; //カウンタ
+    private float _frameCounter; //カウンタ
     [SerializeField] private float _attackStartUpTime;//攻撃が発生前の時間
     [SerializeField] private float _attackRecoveryTime;//攻撃が終わった後の硬直時間
 
     private bool _wasAttack = false;//アタックをしたか
+
+    public override void Enter()
+    {
+        _frameCounter = 0;//初期化
+        _wasAttack = false;
+    }
 
     /// <summary>
     /// カウンタを回して、行動を管理
@@ -30,7 +36,6 @@ public class PlayerAttack : PlayerState
         if(_frameCounter >= _attackStartUpTime)
         {
             Attack();
-            _wasAttack = true;
             _frameCounter = 0;
         }
     }
@@ -40,6 +45,8 @@ public class PlayerAttack : PlayerState
     /// </summary>
     private void Attack()
     {
+        Debug.Log("aa");
         Instantiate(_attackPrefab, player.transform.position, player.transform.rotation);
+        _wasAttack = true;
     }
 }

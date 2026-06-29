f5edaf6f93a6d913762b65c6f3c41cf9e56d8bd6
pawn16124 <kento.monkey124@gmail.com>
2026-06-29 12:21:00 +0900

    DroppingSeelとPeel行動の仕様修正
    ##追加
    - プレイヤーの剥離行動成功時のまとめて取得範囲
    - DroppingSealにChain処理を追加。剥離処理時に実行し再帰的に連鎖して剥がせるシールを探索する

--- DECO-Min/Assets/Scripts/GameObject/Character/Player/Player.cs
+++ DECO-Min/Assets/Scripts/GameObject/Character/Player/Player.cs
@@ -114,8 +114,10 @@ public partial class Player : Character
     }
 
     // ƒV[ƒ‹‘Œ¸ˆ—
-    public void AddSeal(int amount)
+    public void AddSeal(int amount, bool isAddTortal)
     {
+        if(isAddTortal)
+            _status.TotalSealCount += amount;
         _status.CurrentSealCount += amount;
     }
 

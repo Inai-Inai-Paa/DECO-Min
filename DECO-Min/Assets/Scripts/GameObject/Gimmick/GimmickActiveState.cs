f787e838fd57e9ff6ae16dd6083ddbc55069fef6
HAL-Kubodera <ths30024@ths.hal.ac.jp>
2026-06-29 15:26:34 +0900

    ギミックの基底とか (#17)

---
+++ DECO-Min/Assets/Scripts/GameObject/Gimmick/GimmickActiveState.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State/Gimmick")]
+public class GimmickActiveState : GimmickState
+{
+    /// <summary>
+    /// カウンタを回して、行動を管理
+    /// </summary>
+    public override void FixedUpdate()
+    {
+
+    }
+}
